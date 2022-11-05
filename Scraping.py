9f4058bd9a408514cb6f0c4537e3834047a0a3bb
Pasindu Senanayake <pasindusenanayake@iclod.com>
2022-11-06 05:18:13 +0530

    Scraping part code added!

---
+++ Scraping.py
@@ -0,0 +1,32 @@
+import requests, openpyxl
+from bs4 import BeautifulSoup
+
+excel = openpyxl.Workbook()
+sheet = excel.active
+sheet.title = 'Top Rated 250 Movies'
+sheet.append(['Movie Rank', 'Movie Name', 'Released Year', 'IMBD Rating'])
+
+try:
+
+    source = requests.get('https://www.imdb.com/chart/top/')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+    movies = soup.find('tbody', class_="lister-list").find_all('tr')
+
+    for movie in movies:
+
+        name = movie.find('td', class_="titleColumn").a.text
+        rank = movie.find('td', class_="titleColumn").get_text(strip=True).split('.')[0]
+        year = movie.find('td', class_="titleColumn").span.text.strip('()')
+        rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
+
+        print(rank, name, year, rating)
+    
+        sheet.append([rank, name, year, rating])
+        
+
+except Exception as e:
+    print(e)
+
+excel.save('IMDB.xlsx')
